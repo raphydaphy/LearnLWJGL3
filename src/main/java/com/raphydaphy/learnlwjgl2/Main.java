c4189d46e3a63602fcb6e916da3da859bc228e96
raphydaphy <ster2002>
2018-03-07 14:19:10 +1100

    Add gitignore

--- src/main/java/com/raphydaphy/learnlwjgl2/Main.java
+++ src/main/java/com/raphydaphy/learnlwjgl2/Main.java
@@ -24,7 +24,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        DisplayManager.createDisplay("LearnLWJGL3");
+        DisplayManager.createDisplay("LearnLWJGL2");
 
         Loader loader = new Loader();
         Random rand = new Random();
